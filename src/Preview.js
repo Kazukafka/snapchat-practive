2ea945fc77260d7e2eb88fc555d0ad2b75d5d4c5
Kazukafka <pasuwaado135@gmail.com>
2021-10-15 11:46:19 +0900

    uuid error fixed

--- src/Preview.js
+++ src/Preview.js
@@ -5,6 +5,17 @@ import { resetCameraImage, selectCameraImage } from './features/cameraSlice';
 import "./Preview.css";
 import CloseIcon from '@mui/icons-material/Close';
 import { useDispatch } from "react-redux";
+import TextFieldsIcon from '@mui/icons-material/TextFields';
+import CreateIcon from '@mui/icons-material/Create';
+import NoteIcon from '@mui/icons-material/Note';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import CropIcon from '@mui/icons-material/Crop';
+import TimerIcon from '@mui/icons-material/Timer';
+import SendIcon from '@mui/icons-material/Send';
+import { v4 as uuid } from 'uuid';
+import { db, storage } from './firebase';
+import firebase from 'firebase';
 
 function Preview() {
   const cameraImage = useSelector(selectCameraImage);
@@ -22,10 +33,56 @@ function Preview() {
     dispatch(resetCameraImage());
   };
 
+  const sendPost = () => {
+    const id = uuid();
+    const uploadTask = storage
+      .ref(`posts/${id}`)
+      .putString(cameraImage, "data_url");
+
+    uploadTask.on(
+      "state_changed",
+      null,
+      (error) => {
+        //error func
+        console.log(error);
+      },
+      () => {
+        //Complete func
+        storage
+          .ref("posts")
+          .child(id)
+          .getDownloadURL()
+          .then((url) => {
+            db.collection("posts").add({
+              imageUrl: url,
+              username: "TEST",
+              read: false,
+              //profile picture
+              timstamp: firebase.firestore.FieldValue.serverTimestamp(),
+            });
+            history.replace("/chats");
+          });
+      }
+    );
+  };
+
   return (
     <div className="preview">
       <CloseIcon onClick={closePreview} className='preview__close' />
+      <div className="preview__toolbarRight">
+        <TextFieldsIcon />
+        <CreateIcon />
+        <NoteIcon />
+        <MusicNoteIcon />
+        <AttachFileIcon />
+        <CropIcon />
+        <TimerIcon />
+      </div>
       <img src={cameraImage} alt="" />
+      <div onClick={sendPost} className="preview__footer">
+        <h2>Send Now</h2>
+        <SendIcon fontSize="small" className="preview__sendIcon" />
+      </div>
     </div>
   );
 }
